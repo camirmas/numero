94acaaba371adc42dfffb4d7837f526fb2fc7399
Cameron Irmas <cirmas@protonmail.com>
2021-07-10 11:30:21 -0700

    Add bisection tests

--- src/NumericalMethods.jl
+++ src/NumericalMethods.jl
@@ -1,5 +1,9 @@
 module NumericalMethods
 
 include("derivative.jl")
+include("ch5.jl")
+
+export derivative
+export bisect, false_position
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
 using Test
-using NumericalMethods: derivative
+using NumericalMethods
 
 @testset "derivatives" begin
     @test derivative((x) -> 2*x, 2) ≈ 2
     @test derivative((x) -> 2*x, 2, "forward") ≈ 2
     @test derivative((x) -> 2*x, 2, "backward") ≈ 2
     @test derivative((x) -> x^2, 2) ≈ 4
+end
+
+@testset "root finding" begin
+    @test bisect((x) -> x^2-1, 0, 2, .5, 10, 0) ≈ 1
+    @test false_position((x) -> x^2-1, 0, 2, 0, 100, .5) ≈ 1
 end
